ef9c1da1bd631231238194128f078d6f9d0e23ff
EvelinaChumak <chucha.lina.0@gmail.com>
2022-01-27 19:19:22 +0300

    add test for age

--- test_random_user.py
+++ test_random_user.py
@@ -1,7 +1,7 @@
-from models.user import User
 from config.info_for_api import InfoForProject
 import pytest
 import re
+import datetime
 
 
 class TestRandomUser():
@@ -16,10 +16,20 @@ class TestRandomUser():
             'Национальности {} нет в списке {}'.format(
                 [user.nat for user in users], InfoForProject.NAT)
 
-        print([user.phone for user in users])
         m = re.fullmatch(InfoForProject.NUMBER_FORMAT, users[0].phone)
-        print()
         assert all(True if re.fullmatch(InfoForProject.NUMBER_FORMAT_R,
                    user.phone) else False for user in users), \
             'Номера {} не соотвествуют формату {}'.format(
                 [user.phone for user in users], InfoForProject.NUMBER_FORMAT)
+
+
+    def test_age_picture(self, get_users):
+        users = get_users
+        users_date = [datetime.date(
+            *(int(_) for _ in user.dob['date'].split('T')[0].split('-'))) for user in users]
+        users_age = [user.dob['age'] for user in users]
+        current_date = datetime.date.today()
+        our_users_age = [int((current_date-user_date).days/365)
+                         for user_date in users_date]
+        assert users_age == our_users_age, 'Возрост с сайта: {}, возрост на текущий момент: {}'.format(
+            users_age, our_users_age)
