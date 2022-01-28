1dc97b1cbe8f97be3068a1b25e3ff41be6610254
EvelinaChumak <chucha.lina.0@gmail.com>
2022-01-28 11:45:41 +0300

    add picteres and email test

--- conftest.py
+++ conftest.py
@@ -1,8 +1,18 @@
 import pytest
+import json
 from models.user import User
 from config.info_for_api import InfoForProject
 
 
 @pytest.fixture(scope="session")
-def get_users():
-    yield User.get_by_attr(results=InfoForProject.RESILTS)
+def get_users_create_json():
+    users = User.get_by_attr(results=InfoForProject.RESILTS)
+    yield users
+
+    res_json = {}
+    for user in users:
+        res_json[str((user.name['last'], user.email))
+                 ] = user.phone.replace('-', '')
+
+    with open('result.json', 'w') as outfile:
+        json.dump(res_json, outfile, indent=4)
--- test_random_user.py
+++ test_random_user.py
@@ -6,8 +6,8 @@ import datetime
 
 class TestRandomUser():
 
-    def test_user_nat_phohe(self, get_users):
-        users = get_users
+    def test_user_nat_phohe(self, get_users_create_json):
+        users = get_users_create_json
 
         assert len(users) == InfoForProject.RESILTS, 'Получено: {}, ожидалось: {}'.format(
             len(users), InfoForProject.RESILTS)
@@ -22,9 +22,8 @@ class TestRandomUser():
             'Номера {} не соотвествуют формату {}'.format(
                 [user.phone for user in users], InfoForProject.NUMBER_FORMAT)
 
-
-    def test_age_picture(self, get_users):
-        users = get_users
+    def test_age_picture_email(self, get_users_create_json):
+        users = get_users_create_json
         users_date = [datetime.date(
             *(int(_) for _ in user.dob['date'].split('T')[0].split('-'))) for user in users]
         users_age = [user.dob['age'] for user in users]
@@ -33,3 +32,12 @@ class TestRandomUser():
                          for user_date in users_date]
         assert users_age == our_users_age, 'Возрост с сайта: {}, возрост на текущий момент: {}'.format(
             users_age, our_users_age)
+
+        pictures_value = [' ' if user.picture['value']
+                          is None else user.picture['value'][0] for user in users]
+        assert len(set(pictures_value)) == 1, 'Изображения начинаются с символов {}'.format(
+            pictures_value)
+
+        email = [user.email for user in users]
+        assert len(set(email)) == len(
+            users), 'Email не уникальны {}'.format(email)
